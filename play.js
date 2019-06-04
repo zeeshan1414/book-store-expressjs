1ffa2ef5627750ac30775240aeff3a044cd729a4
Zeeshan <zeeshan1414@gmail.com>
2019-06-05 00:25:04 +0530

    Initial commit

---
+++ play.js
@@ -0,0 +1,13 @@
+// let name = "Zeeshan";
+// let age = 20;
+// let hasHobbies = true;
+// age = 21;
+// summarizeUser = (name, age, hobbies) => {
+//   return "Name is " + name + " Age is " + age + " and has hobbies: " + hobbies;
+// };
+
+// console.log(summarizeUser(name, age, hasHobbies));
+
+// setTimeout(() => {
+//   console.log("After 2 sec");
+// }, 2000);
